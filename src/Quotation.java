3dc2689aa7b426f059cfc6b94a1aab85221fb48b
Sssanji <90037657+Sssanji@users.noreply.github.com>
2022-01-21 09:32:54 +0000

    .

--- src/Quotation.java
+++ src/Quotation.java
@@ -1,5 +1,5 @@
 public class Quotation {
-  String quote = "Welcome to Sun!";
+  String quote = "Either you run the day, or the day runs you.";
   public void display() {
     System.out.println(quote);
   }
